4d369ba92d921956a077cec9cdad29d84677578e
Erick Carvalho <erickzcarvalho25@outlook.com>
2026-07-12 21:47:38 -0300

    new commands and removal of unused libs

---
+++ src/commands/fun/boop.ts
@@ -0,0 +1,13 @@
+import { Message } from "discord.js";
+import { Command } from "../../types";
+
+const boopCommand: Command = {
+  name: 'boop',
+  description: 'Boops the specified use, as many times as you want.',
+
+  async execute(message: Message) {
+    
+  }
+};
+
+export default boopCommand;
